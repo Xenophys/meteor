c47e1cc3a848be7f793479f09262dbfa5a437599
Xenophys <thb@baumann-sb.de>
2014-12-17 22:56:18 +0100

    Mark selected player

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -1,11 +1,24 @@
 PlayersList = new Mongo.Collection('players');
 if (Meteor.isClient){
-  Template.leaderboard.helpers({
-    'player' : function(){
-      return PlayersList.find({}, {sort: {score:-1}});
-    }
-  });
-}
+	Template.leaderboard.helpers({
+		'player' : function(){
+				return PlayersList.find({}, {sort: {score:-1}});
+    	},
+    	'selectedClass' : function () {
+			var playerId = this._id;
+			var selectedPlayer = Session.get('selectedPlayer')    		
+			if (playerId == selectedPlayer){
+    			return 'selected'			
+			}    		
+    	}
+  	});
+  	Template.leaderboard.events({
+  		'click .player' : function(){
+  			var playerId = this._id;
+  			Session.set('selectedPlayer',playerId);
+  		}	
+  	});
+} //isClient
 if (Meteor.isServer){
   
-}
+} //isServer
