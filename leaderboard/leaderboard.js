7b9292bb88425deab404d6c024b6559b0d29ae16
Thorsten Baumann <thb@baumann-sb.de>
2014-12-21 22:08:29 +0100

    Added form to add player

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -6,19 +6,27 @@ if (Meteor.isClient){
     	},
     	'selectedClass' : function () {
 			var playerId = this._id;
-			var selectedPlayer = Session.get('selectedPlayer')    		
+			var selectedPlayer = Session.get('selectedPlayer')
 			if (playerId == selectedPlayer){
-    			return 'selected'			
-			}    		
+    			return 'selected'
+			}
     	}
   	});
   	Template.leaderboard.events({
   		'click .player' : function(){
   			var playerId = this._id;
   			Session.set('selectedPlayer',playerId);
-  		}	
+  		}
   	});
+		Template.addPlayerForm.events({
+			'submit form' : function(evt){
+				evt.preventDefault();
+				var playerNameVar=evt.target.playerName.value;
+				PlayersList.insert({name: playerNameVar, score: 0});
+				evt.target.playerName.value="";
+			}
+		});
 } //isClient
 if (Meteor.isServer){
-  
+
 } //isServer
