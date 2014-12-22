af588fa7da3fdca34da7d73e7747fe5be7ccb56a
Thorsten Baumann <thb@baumann-sb.de>
2014-12-22 20:10:26 +0100

    Added functions: give/take points, reset points, delete user

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -2,11 +2,11 @@ PlayersList = new Mongo.Collection('players');
 if (Meteor.isClient){
 	Template.leaderboard.helpers({
 		'player' : function(){
-				return PlayersList.find({}, {sort: {score:-1}});
+				return PlayersList.find({}, {sort: {score: -1, name: 1} });
     	},
     	'selectedClass' : function () {
 			var playerId = this._id;
-			var selectedPlayer = Session.get('selectedPlayer')
+			var selectedPlayer = Session.get('selectedPlayer');
 			if (playerId == selectedPlayer){
     			return 'selected'
 			}
@@ -16,7 +16,27 @@ if (Meteor.isClient){
   		'click .player' : function(){
   			var playerId = this._id;
   			Session.set('selectedPlayer',playerId);
-  		}
+  		},
+			'click .increment' : function(){
+				var selectedPlayer = Session.get('selectedPlayer');
+				PlayersList.update(selectedPlayer, {$inc: {score: 5}});
+			},
+			'click .decrement' : function(){
+				var selectedPlayerId = Session.get('selectedPlayer');
+				var selectedPlayer = PlayersList.findOne(selectedPlayerId);
+				var playerScore = selectedPlayer.score;
+				if (playerScore <= 5){
+					PlayersList.update(selectedPlayer._id, {$set: {score: 0}});
+				} else {
+					PlayersList.update(selectedPlayer._id, {$inc: {score: -5}});
+				}
+			},
+			'click .delete' : function(){
+				PlayersList.remove(this._id);
+			},
+			'click .reset' : function(){
+				var allPlayers = PlayersList.find({}).forEach(function(player){PlayersList.update(player._id, {$set: {score: 0}});});				
+			}
   	});
 		Template.addPlayerForm.events({
 			'submit form' : function(evt){
